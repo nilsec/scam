74be82a7d52acdaad2449c609a65f9bd12fbd545
nilsec <nils.eckstein@googlemail.com>
2021-05-27 16:07:45 -0400

    Add sig, struc params

--- scam/mask.py
+++ scam/mask.py
@@ -7,7 +7,7 @@ from networks import run_inference, init_network
 
 def get_mask(attribution, real_img, fake_img, real_class, fake_class, 
              net_module, checkpoint_path, input_shape, input_nc, output_classes,
-             downsample_factors=None):
+             downsample_factors=None, sigma=11, struc=10):
     """
     attribution: 2D array <= 1 indicating pixel importance
     """
@@ -15,7 +15,7 @@ def get_mask(attribution, real_img, fake_img, real_class, fake_class,
     net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes,
                        downsample_factors=downsample_factors)
     result_dict = {}
-    img_names = ["attr", "real", "fake", "hybrid", "mask_real", "mask_fake", "mask_residual"]
+    img_names = ["attr", "real", "fake", "hybrid", "mask_real", "mask_fake", "mask_residual", "mask_weight"]
     imgs_all = []
     img_thresholds = [0, 0.2, 0.4, 0.6, 0.8, 0.99]
 
@@ -34,12 +34,12 @@ def get_mask(attribution, real_img, fake_img, real_class, fake_class,
         copied_canvas = np.zeros(np.shape(copyfrom))
         mask = np.array(attribution > thr, dtype=np.uint8)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(struc,struc))
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
         mask_size = np.sum(mask)
         mask_cp = copy.deepcopy(mask)
 
-        mask_weight = cv2.GaussianBlur(mask_cp.astype(np.float), (11,11),0)
+        mask_weight = cv2.GaussianBlur(mask_cp.astype(np.float), (sigma,sigma),0)
         copyto = np.array((copyto * (1 - mask_weight)) + (copyfrom * mask_weight), dtype=np.float)
 
         copied_canvas += np.array(mask_weight*copyfrom)
@@ -57,7 +57,7 @@ def get_mask(attribution, real_img, fake_img, real_class, fake_class,
         out_copyto = run_inference(net, im_copied_norm)
 
         imgs = [attribution, real_img_norm, fake_img_norm, im_copied_norm, normalize_image(copied_canvas), 
-                normalize_image(copied_canvas_to), normalize_image(diff_copied)]
+                normalize_image(copied_canvas_to), normalize_image(diff_copied), mask_weight]
 
         #if thr in img_thresholds:
         imgs_all.append(imgs)
