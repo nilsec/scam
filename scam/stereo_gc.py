8c6aac88ed5bbf0aee8fc34859882897394cfe17
nilsec <nils.eckstein@googlemail.com>
2021-05-31 11:25:43 -0400

    Add abs

--- scam/stereo_gc.py
+++ scam/stereo_gc.py
@@ -4,11 +4,11 @@ import os
 import torch
 
 from scam.gradients import get_gradients_from_layer
-from scam.activations import get_activation_dict, get_layer_activations, project_layer_activations_to_input
+from scam.activations import get_activation_dict, get_layer_activations, project_layer_activations_to_input, project_layer_activations_to_input_rescale
 from scam.utils import normalize_image, save_image
 from networks import run_inference, init_network
 
-def get_sgc(real_img, fake_img, real_class, fake_class, net_module, checkpoint_path, input_shape, input_nc, layer_name=None, output_classes=6):
+def get_sgc(real_img, fake_img, real_class, fake_class, net_module, checkpoint_path, input_shape, input_nc, layer_name=None, output_classes=6, downsample_factors=None):
     """
         real_img: Unnormalized (0-255) 2D image
 
@@ -36,23 +36,27 @@ def get_sgc(real_img, fake_img, real_class, fake_class, net_module, checkpoint_p
     classes = [real_class, fake_class]
 
     if layer_name is None:
-        net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes)
+        net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes,
+                           downsample_factors=downsample_factors)
         last_conv_layer = [(name,module) for name, module in net.named_modules() if type(module) == torch.nn.Conv2d][-1]
         layer_name = last_conv_layer[0]
         layer = last_conv_layer[1]
    
     grads = []
     for x,y in zip(imgs,classes):
-        grad_net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes)
+        grad_net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes,
+                                downsample_factors=downsample_factors)
         grads.append(get_gradients_from_layer(grad_net, x, y, layer_name))
 
     acts_real = collections.defaultdict(list)
     acts_fake = collections.defaultdict(list)
 
-    activation_net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes)
+    activation_net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes,
+                                  downsample_factors=downsample_factors)
     acts_real, out_real = get_activation_dict(activation_net, [imgs[0]], acts_real)
 
-    activation_net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes)
+    activation_net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes,
+                                  downsample_factors=downsample_factors)
     acts_fake, out_fake = get_activation_dict(activation_net, [imgs[1]], acts_fake)
 
     acts = [acts_real, acts_fake]
@@ -62,12 +66,19 @@ def get_sgc(real_img, fake_img, real_class, fake_class, net_module, checkpoint_p
     for act in acts:
         layer_acts.append(get_layer_activations(act, layer_name))
 
-    net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes)
+    net = init_network(checkpoint_path, input_shape, net_module, input_nc, eval_net=True, require_grad=False, output_classes=output_classes,
+                       downsample_factors=downsample_factors)
+    #delta_fake = grads[1] * (layer_acts[0] - layer_acts[1])
+    #delta_real = grads[0] * (layer_acts[1] - layer_acts[0])
     delta_fake = grads[1] * (layer_acts[0] - layer_acts[1])
     delta_real = grads[0] * (layer_acts[1] - layer_acts[0])
 
-    delta_fake_projected = project_layer_activations_to_input(net, (input_nc, input_shape[0], input_shape[1]), delta_fake, layer_name)[0,:,:,:]
-    delta_real_projected = project_layer_activations_to_input(net, (input_nc, input_shape[0], input_shape[1]), delta_real, layer_name)[0,:,:,:]
+    #delta_fake_projected = project_layer_activations_to_input(net, (input_nc, input_shape[0], input_shape[1]), delta_fake, layer_name)[0,:,:,:]
+    #delta_real_projected = project_layer_activations_to_input(net, (input_nc, input_shape[0], input_shape[1]), delta_real, layer_name)[0,:,:,:]
+    
+    delta_fake_projected = project_layer_activations_to_input_rescale(delta_fake, (input_shape[0], input_shape[1]))[0,:,:,:]
+    delta_real_projected = project_layer_activations_to_input_rescale(delta_real, (input_shape[0], input_shape[1]))[0,:,:,:]
+
     
     channels = np.shape(delta_fake_projected)[0]
     scam_0 = np.zeros(np.shape(delta_fake_projected)[1:])
@@ -77,6 +88,8 @@ def get_sgc(real_img, fake_img, real_class, fake_class, net_module, checkpoint_p
         scam_0 += delta_fake_projected[c,:,:]
         scam_1 += delta_real_projected[c,:,:]
 
+    scam_0 = np.abs(scam_0)
+    scam_1 = np.abs(scam_1)
     scam_0 /= np.max(np.abs(scam_0))
     scam_1 /= np.max(np.abs(scam_1))
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
